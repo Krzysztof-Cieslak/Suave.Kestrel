785d03fed35d3000b82c007355952feff573320d
Jimmy Byrd <jimmy.byrd@binarydefense.com>
2016-07-24 19:54:52 -0400

    Tweak benchmark.sh with better file names

--- benchmark.sh
+++ benchmark.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+echo "Benchmarking Suave on Kestrel"
 #kestrel
-wrk -t12 -c400 -d30s http://localhost:8080 | tee kestrelbench.txt
+wrk -t12 -c400 -d30s http://localhost:8080 | tee suaveOnKestrel.txt
+echo "Benchmarking Suave On CoreCLR"
 #core clr
-wrk -t12 -c400 -d30s http://localhost:8081 | tee coreclrbench.txt
+wrk -t12 -c400 -d30s http://localhost:8081 | tee suaveOnCoreCLR.txt
+echo "Benchmarking Suave on Mono"
 #mono
-wrk -t12 -c400 -d30s http://localhost:8083 | tee monobench.txt
+wrk -t12 -c400 -d30s http://localhost:8083 | tee suaveOnMono.txt
+echo "Benchmarking Kestrel plain"
 #mono on libuv
-wrk -t12 -c400 -d30s http://localhost:8084 | tee monobenchonlibuv.txt
+wrk -t12 -c400 -d30s http://localhost:8085 | tee kestrelPlain.txt
